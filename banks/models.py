88b360a4475a0144d1d8709191d52ffb80f6f96c
Yuri Rodrigues <yurialves3324@gmail.com>
2025-07-22 00:28:04 -0300

    Criando os modelos de banco e extrato do banco

--- banks/models.py
+++ banks/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Bank(models.Model):
+    
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+
+class BankStatement(models.Model):
+    bank = models.ForeignKey(Bank, on_delete=models.CASCADE)
+    file = models.FileField(upload_to='statements/')
+    upload_at = models.DateTimeField(auto_now_add=True)
+    month = models.DateField()
+    format = models.CharField(max_length=10, 
+                              choices=[('pdf', 'PDF'), ('csv', 'CSV')])
+    
+    
