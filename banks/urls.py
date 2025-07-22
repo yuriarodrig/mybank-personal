1bc3260d771b3a2db1a2c1a7c2b3cd468a509206
Yuri Rodrigues <yurialves3324@gmail.com>
2025-07-22 01:06:40 -0300

    Alteração na url

--- banks/urls.py
+++ banks/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = 'banks'
 
 urlpatterns = [
-    path('', views.bank_list, name='bank_list'), #My list banks
+    path('list', views.bank_list, name='bank_list'), #My list banks
     path('upload/', views.upload_file_bank, name='upload_statement'),  #Upload
 ]
